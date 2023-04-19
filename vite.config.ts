eabc5b79f7be8d785dcf417603be417e543acbac
Hoang Dung <dungnguyen322003@gmail.com>
2023-04-19 18:03:14 +0700

    fix: fix bug

--- vite.config.ts
+++ vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
+import ckeditor5 from '@ckeditor/vite-plugin-ckeditor5/dist';
 
 // https://vitejs.dev/config/
 export default defineConfig({
